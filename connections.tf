c940843be6fe9fdd51512920fad1ab2807890093
Waqar Detho <waqaralidetho@hotmail.com>
2019-09-13 13:34:53 +0200

    build: Add subnetworks to aws cloud

--- connections.tf
+++ connections.tf
@@ -1,16 +1,4 @@
-provider "google" {
-  credentials = "${file("../account.json")}"
-  project = "thrashingcorecode"
-  region = "us-west1"
-}
-
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-central-1"
 }
 
-provider "azurerm" {
-  subscription_id = "0"
-  client_id = "1"
-  client_secret = "2"
-  tenant_id = "3"
-}
